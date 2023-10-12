9d336b7d3425960ce6178b0468351272ad309d9c
Rubio <paulocezarrubio@gmail.com>
2023-10-12 10:58:48 -0300

    test

--- front-end/src/routes/auth/withAuth.js
+++ front-end/src/routes/auth/withAuth.js
@@ -3,11 +3,11 @@ import { getLocalStorage } from '../../utils/localStorage';
 
 const withAuth = (component) => {
   const userData = getLocalStorage('user');
-<<<<<<< HEAD
-=======
-  // console.log(userData?.role === 'customer');
->>>>>>> ddc92a29 (test)
-  return userData?.role === 'customer' ? <Redirect to="/customer/products" /> : component;
+  return userData?.role === 'customer' ? (
+    <Redirect to="/customer/products" />
+  ) : (
+    component
+  );
 };
 
 export default withAuth;
